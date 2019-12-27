756a837f367efc177459ab53d6d66ff91c8c8de0
Sebastián Suárez <sebastian@typeform.com>
2019-12-27 18:39:20 +0100

    Changing github url

--- tmdb.rb
+++ tmdb.rb
@@ -4,7 +4,7 @@ class Tmdb < Formula
   homepage "https://github.com/ssuareza/tmdb"
 
   $v = "v0.0.1"
-  url "https://github.com/ssuareza/tmdb/releases/download/#{$v}/tmdb-#{$v}-darwin-amd64"
+  url "https://github.com/ssuareza/themoviedb-cli/releases/download/#{$v}/tmdb-#{$v}-darwin-amd64"
   version $v
   sha256 "5582347ddbcd73711fc1226b99053f4916e5985e0805e3cd8ac8e2cc80ba2ffa"
 
