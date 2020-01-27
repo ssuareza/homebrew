bfd1a514559c2d8a9579d7935284d48de0cbd4bf
Sebastián Suárez <sebastian@typeform.com>
2020-01-27 09:11:52 +0100

    Fixing version

--- sqs.rb
+++ sqs.rb
@@ -3,7 +3,7 @@ require "FileUtils"
 class Sqs < Formula
   homepage "https://github.com/ssuareza/sqsmover"
 
-  $v = "v0.0.2"
+  $v = "v0.0.1"
   url "https://github.com/ssuareza/sqsmover/releases/download/#{$v}/sqs-#{$v}-darwin-amd64"
   version $v
   sha256 "2afcd9bf324e08f47c86ab7950b0f7a8c18ba56f235f2e6d9672f6f941f503b1"
