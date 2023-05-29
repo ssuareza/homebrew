394ec5f168212bb74e0f3dd9a3f48e929c6e32c1
Sebastián Suárez <sebastian@typeform.com>
2023-05-29 16:20:30 +0200

    Update to tmdb v0.0.4

--- tmdb.rb
+++ tmdb.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Tmdb < Formula
   homepage "https://github.com/ssuareza/tmdb"
 
-  $v = "v0.0.3"
+  $v = "v0.0.4"
   url "https://github.com/ssuareza/themoviedb-cli/releases/download/#{$v}/tmdb-#{$v}-darwin-amd64"
   version $v
-  sha256 "8c331eb29d66b1d7ff3acefd4c4f270fd6b66ce0a3347e8bfc9e3f6c80c36e11"
+  sha256 "9ef9ddd010bfabcb953b9ed8f0cbf64a438e7de594376cb6609b6778ae980245"
 
   def install
     FileUtils.mv("tmdb-#{$v}-darwin-amd64", "tmdb")
